8bc7cde08724612c85b5a25e79a67a64849135be
dot-ma801 <dot.ma801@gmail.com>
2026-05-28 23:49:28 +0900

    [clean] コンポーネントの配置場所を整理

--- packages/frontend/src/components/layout/AppHeader.vue
+++ packages/frontend/src/components/layout/AppHeader.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import ThemeSwitchButton from './ui/button/ThemeSwitchButton.vue';
+import ThemeSwitchButton from '@/components/layout/ThemeSwitchButton.vue';
 </script>
 
 <template>

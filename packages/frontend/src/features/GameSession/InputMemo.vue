07c5820c5b40dd18e0c1ad175561d6c49950cdc8
dot-ma801 <dot.ma801@gmail.com>
2026-06-07 01:03:55 +0900

    [add] メモコンポーネントを追加

---
+++ packages/frontend/src/features/GameSession/InputMemo.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import BaseCard from '@/components/common/BaseCard/BaseCard.vue';
+import BaseSectionHeading from '@/components/common/BaseSectionHeading/BaseSectionHeading.vue';
+import BaseTextArea from '@/components/form/BaseTextArea/BaseTextArea.vue';
+import { MessageSquareText } from '@lucide/vue';
+</script>
+
+<template>
+  <BaseCard>
+    <template #header>
+      <BaseSectionHeading
+        level="h2"
+        :icon="MessageSquareText"
+        text-color="default"
+      >
+        詳細文
+      </BaseSectionHeading>
+    </template>
+
+    <template #default>
+      <BaseTextArea
+        label="メモ"
+        placeholder="共有事項や注意事項、事前準備してほしいことなど…"
+      ></BaseTextArea>
+    </template>
+  </BaseCard>
+</template>
+
+<style scoped>
+.switch {
+  margin-bottom: var(--space-4);
+}
+
+.contents {
+  /* 余白 */
+  > * {
+    margin: var(--space-5) 0;
+
+    &:first-child {
+      margin-top: 0;
+    }
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+}
+</style>
--- packages/frontend/src/views/GameSession/CreateView.vue
+++ packages/frontend/src/views/GameSession/CreateView.vue
@@ -1,22 +1,26 @@
 <script setup lang="ts">
 import BaseSectionHeading from '@/components/common/BaseSectionHeading/BaseSectionHeading.vue';
 import InputBasicInfo from '@/features/GameSession/InputBasicInfo.vue';
+import InputMemo from '@/features/GameSession/InputMemo.vue';
 import InputScheduleInfo from '@/features/GameSession/InputScheduleInfo.vue';
 </script>
 
 <template>
   <div class="container">
-    <BaseSectionHeading level="h1" text-color="primary">セッション新規作成</BaseSectionHeading >
+    <BaseSectionHeading level="h1" text-color="primary">
+      セッション新規作成
+    </BaseSectionHeading>
+
     <InputBasicInfo></InputBasicInfo>
     <InputScheduleInfo></InputScheduleInfo>
+    <InputMemo></InputMemo>
   </div>
 </template>
 
-<style>
-
+<style scoped>
 .container {
-  >* {
-    margin: var(--space-5)
-  }
+  display: flex;
+  flex-direction: column;
+  gap: var(--space-5);
 }
 </style>

6f164ee4d084614cd456e6d6f738fb17bb8b926a
dot-ma801 <dot.ma801@gmail.com>
2026-06-07 01:14:07 +0900

    [clean] ディレクトリの移動

---
+++ packages/frontend/src/features/GameSession/Edit/index.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import BaseButton from '@/components/button/BaseButton.vue';
+import BaseSectionHeading from '@/components/common/BaseSectionHeading/BaseSectionHeading.vue';
+import InputBasicInfo from '@/features/GameSession/Edit/InputBasicInfo.vue';
+import InputMemo from '@/features/GameSession/Edit/InputMemo.vue';
+import InputScheduleInfo from '@/features/GameSession/Edit/InputScheduleInfo.vue';
+</script>
+
+<template>
+  <div class="container">
+    <BaseSectionHeading level="h1" text-color="primary">
+      セッション新規作成
+    </BaseSectionHeading>
+
+    <InputBasicInfo></InputBasicInfo>
+    <InputScheduleInfo></InputScheduleInfo>
+    <InputMemo></InputMemo>
+  </div>
+
+  <div class="button-area">
+    <BaseButton variant="secondary">キャンセル</BaseButton>
+    <BaseButton>セッションを作成する</BaseButton>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  gap: var(--space-5);
+}
+
+.button-area {
+  display: flex;
+  justify-content: flex-end;
+  gap: var(--space-3);
+  margin-top: var(--space-6);
+}
+</style>
--- packages/frontend/src/views/GameSession/CreateView.vue
+++ packages/frontend/src/views/GameSession/CreateView.vue
@@ -1,26 +1,9 @@
 <script setup lang="ts">
-import BaseSectionHeading from '@/components/common/BaseSectionHeading/BaseSectionHeading.vue';
-import InputBasicInfo from '@/features/GameSession/InputBasicInfo.vue';
-import InputMemo from '@/features/GameSession/InputMemo.vue';
-import InputScheduleInfo from '@/features/GameSession/InputScheduleInfo.vue';
+import GameSessionEdit from '@/features/GameSession/Edit/index.vue';
 </script>
 
 <template>
-  <div class="container">
-    <BaseSectionHeading level="h1" text-color="primary">
-      セッション新規作成
-    </BaseSectionHeading>
-
-    <InputBasicInfo></InputBasicInfo>
-    <InputScheduleInfo></InputScheduleInfo>
-    <InputMemo></InputMemo>
-  </div>
+  <GameSessionEdit></GameSessionEdit>
 </template>
 
-<style scoped>
-.container {
-  display: flex;
-  flex-direction: column;
-  gap: var(--space-5);
-}
-</style>
+<style scoped></style>

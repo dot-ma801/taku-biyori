ac98727d2ee67fc5b816f72c580a45fe04851ec9
dot-ma801 <dot.ma801@gmail.com>
2026-06-07 00:00:15 +0900

    [add] 実施情報の入力欄を追加

---
+++ packages/frontend/src/features/GameSession/InputScheduleInfo.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import BaseCard from '@/components/common/BaseCard/BaseCard.vue';
+import BaseSectionHeading from '@/components/common/BaseSectionHeading/BaseSectionHeading.vue';
+import BaseDatePicker from '@/components/form/BaseDatePicker/BaseDatePicker.vue';
+import BaseDateTimePicker from '@/components/form/BaseDateTimePicker/BaseDateTimePicker.vue';
+import BaseSwitch from '@/components/form/BaseSwitch/BaseSwitch.vue';
+import BaseTextBox from '@/components/form/BaseTextBox/BaseTextBox.vue';
+import { CalendarDays } from '@lucide/vue';
+import { ref } from 'vue';
+
+const selectMultiDays = ref(false)
+</script>
+
+<template>
+  <BaseCard>
+    <template #header>
+      <BaseSectionHeading level="h2" :icon="CalendarDays" text-color="default">
+        実施情報
+      </BaseSectionHeading>
+    </template>
+
+    <template #default>
+      <BaseSwitch class="switch" v-model="selectMultiDays" label="複数の候補日を選択する"></BaseSwitch>
+      <div class="contents">
+        <BaseDatePicker  v-if="selectMultiDays" label="候補日" multiple></BaseDatePicker>
+        <BaseDateTimePicker v-else label="開催日時"></BaseDateTimePicker>
+        <BaseTextBox label="実施場所" placeholder="例：Discord + ココフォリア"></BaseTextBox>
+      </div>
+    </template>
+  </BaseCard>
+</template>
+
+<style scoped>
+.switch {
+  margin-bottom: var(--space-4);
+}
+
+.contents {
+  /* 余白 */
+  > * {
+    margin: var(--space-5) 0;
+
+    &:first-child {
+      margin-top: 0;
+    }
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+}
+</style>
--- packages/frontend/src/views/GameSession/CreateView.vue
+++ packages/frontend/src/views/GameSession/CreateView.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import BaseSectionHeading from '@/components/common/BaseSectionHeading/BaseSectionHeading.vue';
 import InputBasicInfo from '@/features/GameSession/InputBasicInfo.vue';
+import InputScheduleInfo from '@/features/GameSession/InputScheduleInfo.vue';
 </script>
 
 <template>
   <div class="container">
     <BaseSectionHeading level="h1" text-color="primary">セッション新規作成</BaseSectionHeading >
     <InputBasicInfo></InputBasicInfo>
+    <InputScheduleInfo></InputScheduleInfo>
   </div>
 </template>
 

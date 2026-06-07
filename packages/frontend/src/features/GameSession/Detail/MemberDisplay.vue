ac0485bf88e666f38af1cc480e2ede6bee0ba147
dot-ma801 <dot.ma801@gmail.com>
2026-06-07 17:12:27 +0900

    [add] 詳細画面の雛形を追加

---
+++ packages/frontend/src/features/GameSession/Detail/MemberDisplay.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import UserAvatar from '@/features/user/UserAvatar/UserAvatar.vue';
+import BaseCard from '@/components/common/BaseCard/BaseCard.vue';
+import BaseSectionHeading from '@/components/common/BaseSectionHeading/BaseSectionHeading.vue';
+import { UsersRound } from '@lucide/vue';
+</script>
+
+<template>
+  <BaseCard>
+    <BaseSectionHeading class="header" level="h3" :icon="UsersRound">
+      参加メンバー
+    </BaseSectionHeading>
+
+    <div v-for="i in 3" :key="i" class="user-container">
+      <UserAvatar class="avatar"></UserAvatar>
+
+      <p class="user-name">{{ '田中' }}</p>
+      <p class="char-name">キャラクター：{{ 'チシャ猫' }}</p>
+    </div>
+  </BaseCard>
+</template>
+
+<style scoped>
+.header {
+  margin-bottom: var(--space-4);
+}
+
+.user-container {
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-template-rows: 1fr 1fr;
+  align-items: center;
+
+  border-top: solid 2px var(--color-border);
+
+  padding: var(--space-2);
+
+  .avatar {
+    grid-column: 1 / 2;
+    grid-row: 1 / 3;
+    margin: var(--space-2);
+  }
+
+  .user-name {
+    grid-column: 2 / 3;
+    grid-row: 1 / 2;
+  }
+
+  .char-name {
+    grid-column: 2 / 3;
+    grid-row: 2 / 3;
+  }
+}
+</style>
---
+++ packages/frontend/src/features/GameSession/Detail/index.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+defineOptions({ name: 'GameSessionDetail' });
+import BaseSectionHeading from '@/components/common/BaseSectionHeading/BaseSectionHeading.vue';
+import MemberDisplay from '@/features/GameSession/Detail/MemberDisplay.vue';
+import MemoDisplay from '@/features/GameSession/Detail/MemoDisplay.vue';
+import ScenarioInfoDisplay from '@/features/GameSession/Detail/ScenarioInfoDisplay.vue';
+import ScheduleDisplay from '@/features/GameSession/Detail/ScheduleDisplay.vue';
+import { ref } from 'vue';
+import { Album, UsersRound } from '@lucide/vue';
+
+const title = ref('セッションタイトル');
+</script>
+
+<template>
+  <div class="container">
+    <div>
+      <BaseSectionHeading level="h1">{{ title }}</BaseSectionHeading>
+      <div class="description">
+        <Album :size="16" />
+        <p>シナリオ：{{}}</p>
+        <UsersRound :size="16" />
+        <p>募集人数: {{}}</p>
+      </div>
+    </div>
+
+    <ScenarioInfoDisplay></ScenarioInfoDisplay>
+    <MemoDisplay></MemoDisplay>
+    <ScheduleDisplay></ScheduleDisplay>
+    <MemberDisplay></MemberDisplay>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  gap: var(--space-5);
+}
+
+.description {
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-template-rows: 1fr 1fr;
+  align-items: center;
+
+  gap: 0 var(--space-1);
+  padding-left: var(--space-3);
+}
+</style>

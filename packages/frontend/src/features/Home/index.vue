c258b9bb5a8d0314ef37cd0e20bedb7864d2776a
dot-ma801 <dot.ma801@gmail.com>
2026-06-28 21:36:11 +0900

    [add] セッション作成ボタンを追加

--- packages/frontend/src/features/Home/index.vue
+++ packages/frontend/src/features/Home/index.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
 defineOptions({ name: 'Home' });
+import BaseButton from '@/components/button/BaseButton.vue';
 import MySessionList from '@/features/Home/MySessionList.vue';
 import PublicSessionList from '@/features/Home/PublicSessionList.vue';
+import { Plus } from '@lucide/vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const onClickCreate = () => {
+  router.push({name: 'game-sessions-new'})
+}
 </script>
 
 <template>
   <div class="container">
+    <BaseButton class="create-btn" :left-icon="Plus" @click="onClickCreate">セッションを作成</BaseButton>
     <MySessionList></MySessionList>
     <PublicSessionList></PublicSessionList>
   </div>
@@ -16,5 +26,9 @@ import PublicSessionList from '@/features/Home/PublicSessionList.vue';
   display: flex;
   flex-direction: column;
   gap: var(--space-4);
+}
+
+.create-btn {
+  align-self: flex-end;
 }
 </style>

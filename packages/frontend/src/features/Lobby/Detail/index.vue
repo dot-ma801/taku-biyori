cd2a406ccc8a45bd3b46e99ef5bd8d39ee35147d
dot-ma801 <dot.ma801@gmail.com>
2026-07-13 23:50:03 +0900

    [add] Lobby 詳細画面のエントリポイント index.vue を追加し DetailView に接続

---
+++ packages/frontend/src/features/Lobby/Detail/index.vue
@@ -0,0 +1,90 @@
+<script setup lang="ts">
+defineOptions({ name: 'LobbyDetail' });
+
+import BaseSectionHeading from '@/components/common/BaseSectionHeading/BaseSectionHeading.vue';
+import ActionBar from '@/features/Lobby/Detail/ActionBar.vue';
+import StatusDisplay from '@/features/Lobby/Detail/StatusDisplay.vue';
+import { useGetLobbyDetail } from '@/features/Lobby/Detail/composables/useGetLobbyDetail';
+import { computed } from 'vue';
+import { Album, UsersRound, CalendarDays } from '@lucide/vue';
+
+const props = defineProps<{ lobbyId: string }>();
+
+const {
+  lobby,
+  loading,
+  errorMessage,
+  fetch,
+  patchLobby,
+  addMember,
+  removeMember,
+  memberCount,
+} = useGetLobbyDetail(props.lobbyId);
+
+const scenarioName = computed(() => {
+  return lobby.value?.scenarioName ?? '未設定';
+});
+
+const gameSessionDateTime = computed(() => {
+  return lobby.value?.closedAt ?? '未設定';
+});
+
+const maxMembers = computed(() => {
+  return lobby.value?.maxPlayers ?? '未設定';
+});
+</script>
+
+<template>
+  <template v-if="lobby">
+    <BaseSectionHeading level="h1">
+      {{ lobby.title }}
+    </BaseSectionHeading>
+
+    <div class="session-meta-bar">
+      <div class="description">
+        <Album :size="16" />
+        <p>シナリオ：{{ scenarioName }}</p>
+        <CalendarDays :size="16" />
+        <p>日時：{{ gameSessionDateTime }}</p>
+        <UsersRound :size="16" />
+        <p>募集人数: {{ maxMembers }}</p>
+      </div>
+
+      <div class="action-bar-wrapper">
+        <ActionBar />
+      </div>
+    </div>
+
+    <StatusDisplay :lobby-status="lobby.status" />
+  </template>
+</template>
+
+<style scoped>
+.session-meta-bar {
+  display: flex;
+  justify-content: space-between;
+
+  .description {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    grid-template-rows: 1fr 1fr;
+    align-items: center;
+
+    gap: 0 var(--space-1);
+    padding-left: var(--space-3);
+  }
+
+  @media (max-width: 780px) {
+    flex-direction: column;
+    gap: var(--space-4) 0;
+
+    .description {
+      padding-left: 0;
+    }
+
+    .action-bar-wrapper {
+      align-self: flex-end;
+    }
+  }
+}
+</style>
--- packages/frontend/src/views/Lobby/DetailView.vue
+++ packages/frontend/src/views/Lobby/DetailView.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
+import PageContainer from '@/components/layout/PageContainer/PageContainer.vue';
+import LobbyDetail from '@/features/Lobby/Detail/index.vue';
 const props = defineProps<{ lobbyId: string }>();
 </script>
 
-<template>detail:{{ props.lobbyId }}</template>
+<template>
+  <PageContainer>
+    <LobbyDetail :lobby-id="props.lobbyId" />
+  </PageContainer>
+</template>
 
 <style scoped></style>

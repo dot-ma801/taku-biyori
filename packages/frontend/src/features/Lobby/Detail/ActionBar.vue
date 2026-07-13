f9ca5cb2a3f0182fd93af927ec73ea28e2df10bf
dot-ma801 <dot.ma801@gmail.com>
2026-07-13 23:49:48 +0900

    [add] Lobby 詳細の操作ボタン群コンポーネント ActionBar を追加

---
+++ packages/frontend/src/features/Lobby/Detail/ActionBar.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import BaseButton from '@/components/button/BaseButton.vue';
+import { Share2, SquarePen, Ban } from '@lucide/vue';
+</script>
+
+<template>
+  <div class="button-area">
+    <BaseButton variant="secondary" :left-icon="SquarePen"> 編集 </BaseButton>
+    <BaseButton :left-icon="Share2" variant="secondary">
+      招待リンクを取得
+    </BaseButton>
+    <BaseButton variant="danger" :left-icon="Ban"> 募集中止 </BaseButton>
+  </div>
+</template>
+
+<style scoped>
+.button-area {
+  > * {
+    margin: 0 var(--space-1);
+  }
+}
+</style>
